98c7f0077be865ff57546e29d2baa3ff2474532f
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-11-20 11:23:25 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (32.40%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        visited = dict() # we need index too, so using dict
+
+        for index, num in enumerate(nums):
+            reminder = target - num
+
+            if reminder in visited:
+                return [visited[reminder], index]
+
+            visited[num] = index
+
+        
