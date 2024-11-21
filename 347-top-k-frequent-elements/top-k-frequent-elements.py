570cfc9dbec9fce88dcc940f2e5a6a8bb98a02ea
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-11-21 08:46:04 +0530

    Time: 2 ms (93.50%) | Memory: 20.2 MB (83.13%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -3,10 +3,12 @@ class Solution:
         freq = defaultdict(int)
 
         for num in nums:
-            freq[num] += 1
+            freq[num] += 1 # calculate frequency of each
 
+        # sort based on the value in descending order
         desc_sort = sorted(freq.items(), key= lambda item: item[1], reverse=True)
 
+        # pick the most k elements
         result = [desc_sort[i][0] for i in range(k)]
 
         return result
