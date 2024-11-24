c0dcc8d6fccf857b157456195696f7817ac34f71
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2024-11-24 15:19:40 +0530

    Time: 3 ms (91.31%) | Memory: 20.2 MB (81.64%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -3,6 +3,23 @@ class Solution:
         freq = defaultdict(int)
 
         for num in nums:
+            freq[num] += 1 # calculate frequency
+
+        heap = [] # push the freq (count, num) to heap and maintain the k-size, as we need only k elements
+        for num, count in freq.items():
+            heapq.heappush(heap, (count, num))
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        result = [pair[1] for pair in heap]
+
+        return result
+
+    def topKFrequentUsingSorting(self, nums: List[int], k: int) -> List[int]:
+        freq = defaultdict(int)
+
+        for num in nums:
             freq[num] += 1 # calculate frequency of each
 
         # sort based on the value in descending order
